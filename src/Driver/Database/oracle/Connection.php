4109fc4604f911c6e516923c8422f9263f78118e
Fabian Franz <fabianfranz.oss@gmail.com>
2022-05-20 10:26:53 +0200

    Revert "Issue #3245880: Use deprecated Statement for now."
    This reverts commit 2cfdfa16efa62d65163d65675a2bd1b3e8e0ab64.

--- src/Driver/Database/oracle/Connection.php
+++ src/Driver/Database/oracle/Connection.php
@@ -8,7 +8,6 @@ use Drupal\Core\Database\Database;
 use Drupal\Core\Database\Connection as DatabaseConnection;
 use Drupal\Core\Database\Log;
 use Drupal\Core\Database\StatementInterface;
-use Drupal\oracle\Driver\Database\oracle\Statement;
 use Drupal\oracle\Driver\Database\oracle\StatementWrapper;
 
 /**
@@ -180,7 +179,7 @@ class Connection extends DatabaseConnection {
   /**
    * {@inheritdoc}
    */
-  protected $statementClass = Statement::class;
+  protected $statementClass = NULL;
 
   /**
    * {@inheritdoc}
